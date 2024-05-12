98d257258d56e228465eec656bb675285d2b3cee
harshp4114 <harshpatadia4114@gmail.com>
2024-05-12 19:00:46 +0530

    speed and accuracy calculated correctly and made the test timer based

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-let resetBtn = document.querySelector("#resetBtn");
-let newBtn = document.querySelector("#newBtn");
-let randomNum = Math.floor(Math.random() * 10);
-let textSpeech = document.querySelector(".test-speech");
 let randomSentences = [
     ["After a week of rain, the sun finally emerged from behind the clouds, casting a warm glow over the city and lifting everyone's spirits."],
     ["She decided to redecorate her bedroom, painting the walls a soft shade of blue and adding cozy blankets and fluffy pillows to create a relaxing atmosphere."],
@@ -14,74 +10,146 @@ let randomSentences = [
     ["After years of hard work and dedication, he finally achieved his dream of starting his own business, feeling proud and grateful for the opportunity to pursue his passions."],
     ["They rented a cabin in the mountains for a weekend getaway, hiking through lush forests, roasting marshmallows over a campfire, and stargazing under the clear night sky."]
 ];
-let testHeading = randomSentences[randomNum];
-let textArea = document.querySelector(".text-field");
-let correctChr = 0;
-let totalchr = testHeading[0].length;
-let flag = true;
-let initialMins = 0, initialSec = 0, finalMins = 0, finalSec = 0;
-let resultSpeed = document.querySelector(".result-speed");
-let speed = 0;
-let j = 0;
-textSpeech.innerText = randomSentences[randomNum];
-let result1 = document.querySelector("#result1");
-let result2 = document.querySelector("#result2");
 
-// if(textArea.innerText!=""){
-textArea.addEventListener("input", () => {
-    // console.log("hii");
-    let stringTest = textArea.value;
-    if (flag) {
-        flag = false;
-        let currTime = new Date();
-        initialMins = currTime.getMinutes();
-        initialSec = currTime.getSeconds();
-        // console.log(currTime);
-        // console.log(initialMins);
-        // console.log(initialSec);
 
+let resetBtn = document.querySelector("#resetBtn");     // selecting reset button
+let newBtn = document.querySelector("#newBtn");     // selecting new text button 
+let testSpeech = document.querySelector(".test-speech");    // selecting test speech 
+let textArea = document.querySelector(".text-field");       // selecting text field area
+let resultSpeed = document.querySelector(".result-speed");      // selecting div for result
+let result1 = document.querySelector("#result1");       // selecting p for speed
+let result2 = document.querySelector("#result2");       // selecting p for accuracy
+let timer = document.querySelector(".countdown");     // selecting timer paragraph tag
+let randomNum = Math.floor(Math.random() * 10);     //generating random number
+testSpeech.innerText = randomSentences[randomNum];      // setting test sentence 
+let testHeading = randomSentences[randomNum][0];
+const STARTING_SEC = 2;
+let time = STARTING_SEC;
+let intervalID = 0;
+let correctWords=0;
+let totalWords=testHeading.split(" ").length;
+let userString="";
+let masterString="";
+let totalChr=0;
+
+
+// new text is generated when button is clicked
+newBtn.addEventListener("click", () => {
+    let num = Math.floor(Math.random() * 10);
+    while (num == randomNum) {
+        num = Math.floor(Math.random() * 10);
     }
-    // console.log("hiiii");
-    // console.log(stringTest);
-    for (let i = j; i < stringTest.length; i++) {
-        if (stringTest[i] == testHeading[0][i]) {
-            // console.log("hii");
-            // let charSpan = document.createElement("span");
-            // charSpan.style.color="green";
-            // charSpan.textContent=stringTest[i];
-            // let string=stringTest;
-            // let updatedString=stringTest.substring(0,i)+charSpan.outerHTML+stringTest.substring(i+1);
-            // textArea.innerHTML=updatedString;
-            // string=
-            j++;
-            correctChr++;
-            console.log(`i is : ${i}`);
-            console.log(`correctchr is : ${correctChr}`);
-            console.log(`totalchr is : ${totalchr}`);
+    testSpeech.innerText = randomSentences[num];
+    textArea.disabled=false;
+    timer.style.marginTop="1.65rem";
+    timer.style.marginBottom="1.5rem";
+    textArea.style.marginTop="35px";
+    textArea.style.marginBottom="35px";
+    textArea.style.height="30%";
+    resultSpeed.style.display="none";
+    textArea.value="";
 
-        } else {
-        }
-        if ((i + 1) == totalchr) {
-            console.log("hiiiiii");
-            textArea.disabled=true;
-            let d = new Date();
-            finalMins = d.getMinutes();
-            finalSec = d.getSeconds();
-            let timeTaken = (finalMins - initialMins) * 60 + (finalSec - initialSec);
-            speed = Math.abs(Math.floor(timeTaken * (correctChr / totalchr)));
-            console.log(speed);
-            textArea.style.height = "17%";
-            let accuracy = (correctChr / totalchr) * 100;
-            console.log(accuracy);
-            if (speed) {
-                result1.innerText = `Your typing speed is ${speed} characters per minute.`;
-            }
-            result2.innerText = `Your typing accuracy is ${accuracy}%.`;
-            resultSpeed.style.display = "flex";
-        }
-    }
 })
-// }
+
+
+//function to find total characters in the user input excluding white spaces
+function findTotalChr(){
+    let splitString=textArea.value.trim().split(" ");
+    let count=0;
+    for(let i=0;i<splitString.length;i++){
+        count=count+splitString[i].length;
+    }
+    return count;
+}
+
+
+//function to calculate result and accuracy
+function checkResult() {
+     userString = ((textArea.value).trim()).split(" ");
+     masterString = testHeading.split(" ");
+    totalChr=findTotalChr();
+    let correctChr=0;
+    for(let i=0;i<Math.min(userString.length,masterString.length);i++){
+        for(let j=0;j<Math.min(userString[i].length,masterString[i].length);j++){
+            if(userString[i][j]==masterString[i][j]){
+                correctChr++;
+                // console.log("correctChr");
+            }
+
+        }
+    }
+    for(let i=0;i<Math.min(userString.length,masterString.length);i++){
+        if(userString[i] === masterString[i]){
+            correctWords++;
+        }else{
+
+        }
+    }
+    newBtn.style.display="inline-block";
+    let accuracy=Math.floor((correctChr/totalChr)*100);
+    textArea.style.height="17%";
+    timer.style.marginTop="1rem";
+    timer.style.marginBottom="0rem";
+    textArea.style.marginTop="24px";
+    textArea.style.marginBottom="24px";
+    result1.innerText=`Your typing speed is ${correctWords*2} words per minute`;
+    result2.innerText=`Your typing accuracy is ${accuracy}%`; 
+    resultSpeed.style.display="inline-block"
+}
+
+
+//countdown interval function
+function countDown() {
+    if (time < 0) {
+        textArea.disabled=true;
+        timer.innerText="TIME OUT!!!"
+        clearInterval(intervalID);
+        checkResult();
+    } else {
+        let string = `TIMER : ${time} SEC`;
+        timer.innerText = string;
+        time--;
+    }
+}
+
+
+//event listener to remove new text btn once user is giving some input
+textArea.addEventListener("input", () => {
+    newBtn.style.display="none";
+});
+
+
+//starting the timer countdown using the event listener only once
+function timerStart() {
+    intervalID = setInterval(countDown, 1000);
+    textArea.removeEventListener("input", timerStart);
+}
+textArea.addEventListener("input", timerStart);
+
+
+
+
+//timer is reset and textarea is cleared when reset button is clicked
+resetBtn.addEventListener("click", () => {
+    time = STARTING_SEC;
+    textArea.value = "";
+    timer.innerText = "TIMER : 30 SEC";
+    newBtn.style.display="inline-block";
+    resultSpeed.style.display="none";
+    textArea.style.height="30%";
+    timer.style.marginTop="1.65rem";
+    timer.style.marginBottom="1.5rem";
+    textArea.style.marginTop="35px";
+    textArea.style.marginBottom="35px";
+    textArea.disabled=false;
+    clearInterval(intervalID);
+    textArea.addEventListener("input", timerStart);
+    correctWords=0;
+
+});
+
+
+//changing colour of buttons when hover action in performed on them
 resetBtn.addEventListener("mouseover", () => {
     resetBtn.style.backgroundColor = "#9D4EDD";
     resetBtn.style.color = "#10002B";
@@ -103,5 +171,3 @@ newBtn.addEventListener("mouseout", () => {
     newBtn.style.color = "#E0AAFF";
 
 });
-
-
